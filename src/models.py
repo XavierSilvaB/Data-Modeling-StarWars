7b08691487b754c5903281fda16b81bcad297ad7
XavierSilvaB <xavieraugustosilvabaute@gmail.com>
2022-08-22 00:33:54 +0000

    ejercicio Data-Modeling-StarWars

--- src/models.py
+++ src/models.py
@@ -2,32 +2,70 @@ import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
 from eralchemy import render_er
+
+
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+#Table de usuarios
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_name = Column(String(30), nullable=False, unique=True)
+    email = Column(String(80), nullable=False, unique=True)
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    def verifyLogin(self):
+        """verify login"""
+    
+    def addToFavorite(self):
+        """Add to Favorite """
 
-    def to_dict(self):
-        return {}
+    def removeFavorite(self):
+        """remove Favorite"""
+#Table de favoritos
+# relacionada con las tablas de : personajes , planeta y vehiculos
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    Vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+
+#Table de personajes
+class Character(Base):
+    __tablename__= 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False, unique=True)
+    gender = Column(String(40), nullable=False)
+    height = Column(Integer, nullable=False)
+    species = Column(String(100), nullable=False)
+    weapons = Column(String(100), nullable=False)
+    affiliations = Column(String(100), nullable=False)
+
+
+#Table de Vehiculos
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False, unique=True)
+    crew = Column(String(80), nullable=False)
+    passenger = Column(String(80), nullable=False)
+    vehicle_class = Column(String(80), nullable=False)
+    model = Column(String(80), nullable=False)
+
+#Table de Planetas
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False, unique=True)
+    climate = Column(String(80), nullable=False)
+    gravity = Column(String (80), nullable=False) 
+    diameter = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)  
+    terrain = Column(String(80), nullable=False)
+ 
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
